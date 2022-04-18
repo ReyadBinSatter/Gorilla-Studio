c393579fae2cf225a3a5a35a6c0bc7c04b3386c2
ReyadBinSatter <rbsreyad@gmail.com>
2022-04-18 21:28:43 +0600

    create about

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,9 +1,22 @@
 import React from 'react';
-
+import rbs from '../../img/profile/profile.jpg'
+import './About.css';
 const About = () => {
     return (
-        <div>
-            <h1 className='bg-dark'>hi Rbs</h1>
+        <div className='about-me'>
+            <img className='mx-auto d-block' src={rbs} alt="" />
+            <h1 className='text-center'>I'm Reyad</h1>
+ 
+           <h3 className='text-center'>My next goals are-</h3>
+           <p className='text-center w-50 mx-auto'>
+        Become an expert in programming languages JavaScript and Python.
+            Strengthen data management skills by working with DBMS systems.
+            Secure a position as a lead software engineer.
+            Gain enough experience within my current role to be able to apply for senior engineer.
+        </p>
+            
+            
+        
         </div>
     );
 };
